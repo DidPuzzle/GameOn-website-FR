504540012548596ff18757ca309ab8436fd74d15
Didier <te-busco@live.fr>
2023-12-09 15:54:46 +0100

    Fermeture du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -2,22 +2,33 @@
 
 // DOM Elements
 const modalBackground = document.querySelector(".bground");
-const modalButttons = document.querySelectorAll(".modal-btn");
+const modalButtons = document.querySelectorAll(".modal-btn");
 const formInputs = document.querySelectorAll(".formData");
+const closeModalButton = document.querySelector(".close");
+
+if(closeModalButton == null) throw new Error("No close button found");
 
 // launch modal event
 modalButtons.forEach((btn) => btn.addEventListener("click", launchModal));
+closeModalButton.addEventListener("click",closeModal);
+
+function closeModal() {
+  if(modalBackground == null) throw new Error("No modal background found");
+  modalBackground.classList.remove("active");
+}
 
 // launch modal form
 function launchModal() {
+  if(modalBackground == null) throw new Error("No modal background found");
+  modalBackground.classList.add("active");
 
-  modalBackground.style.display = "block";
+
 }
 
 function editNav() {
   const topNavbar = document.getElementById("myTopnav");
   if(topNavbar == null) throw new Error("No topNavbar found");
-  topNavbar.className.toggle("responsive");
+  topNavbar.classList.toggle("responsive");
   
 }
 
