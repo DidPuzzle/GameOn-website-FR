7a818d0a1bd8a58901ad60cfc6a69eb5ad927162
Didier <te-busco@live.fr>
2023-12-09 18:35:42 +0100

    acceptation des conditions

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -5,12 +5,26 @@ const modalBackground = document.querySelector(".bground");
 const modalButtons = document.querySelectorAll(".modal-btn");
 const formInputs = document.querySelectorAll(".formData");
 const closeModalButton = document.querySelector(".close");
+const conditionsAcceptedCheckbox = document.querySelector("#checkbox1");
+const submitButton = document.querySelector(".btn-submit");
+
 
 if(closeModalButton == null) throw new Error("No close button found");
+if(conditionsAcceptedCheckbox == null) throw new Error("no checkbox found");
+
+conditionsAcceptedCheckbox.addEventListener("change", checkIfConditionsAccepted);
 
 // launch modal event
 modalButtons.forEach((btn) => btn.addEventListener("click", launchModal));
 closeModalButton.addEventListener("click",closeModal);
+
+function checkIfConditionsAccepted(event){
+  if(submitButton == null) throw new Error("No submit-button found");
+  const isChecked = event.target.checked;
+  if(isChecked) return submitButton.removeAttribute("disabled");
+  submitButton.setAttribute("disabled","true");
+  
+}
 
 function closeModal() {
   if(modalBackground == null) throw new Error("No modal background found");
